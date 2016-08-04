0f03b9abcfb6c4a10ed3c4f160dd3dbb97d227df
LDENV Ruby <dietertim@mac.com>
2016-08-04 21:24:42 +0200

    Add review model, migrations and model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  
-  has_many :plays
 
+  has_many :plays
+  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
 end
